d738a7d616f5cc7090fa0809731ceb04efc905ec
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-03-11 23:06:15 +0200

    done

--- I.2/SO/lab4/10.sh
+++ I.2/SO/lab4/10.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-n=$1
-k=$(( ($#-1)/$n ))
-p=$(( ($#-1)%$n ))
-
-for ((i=0; i<$k; i++))
-do
-  sum=0
-  for ((j=1; j<=$n; j++))
-  do
-    position=$((1+(i*n)+j))
-    sum=$(( $sum+${!position} ))
-  done
-  echo $sum
-  echo $sum >> output.txt
-done
-
-if [ $p -ne 0 ]
+if [ $# -eq 0 ]
 then
-  sum=0
-  for ((i=1; i<=$p; i++))
+  read n
+  for ((i=1; i<=$n; i++))
   do
-    position=$((position+1))
-    sum=$(( $sum+${!position} ))
+    read i
+    sum=$(($sum+$i))
   done
   echo $sum
-  echo $sum >> output.txt
+else
+  n=$1
+  k=$(( ($#-1)/$n ))
+  p=$(( ($#-1)%$n ))
+
+  for ((i=0; i<$k; i++))
+  do
+    sum=0
+    for ((j=1; j<=$n; j++))
+    do
+      position=$((1+(i*n)+j))
+      sum=$(( $sum+${!position} ))
+    done
+    echo $sum
+    echo $sum >> output.txt 
+  done
+
+  if [ $p -ne 0 ]
+  then
+    sum=0
+    for ((i=1; i<=$p; i++))
+    do
+      position=$((position+1))
+      sum=$(( $sum+${!position} ))
+    done
+    echo $sum
+    echo $sum >> output.txt
+  fi
 fi
