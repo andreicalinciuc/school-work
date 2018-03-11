4d3ecd237bca525e1f1c6424a287873d6f144200
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-03-11 17:36:04 +0200

    Create lab4.8.sh

---
+++ I.2/SO/lab4.8.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+voturi=0
+candidat=0
+
+for ((i=1; i<=$#; i++))
+do
+  array[${!i}]=$((${array[${!i}]}+1))
+  if ((${!i}>maxi))
+  then
+    maxi=${!i}
+  fi
+done
+
+echo ${array[9]}
+
+for ((i=1; i<=$#; i++))
+do
+  if ((${array[${!i}]}>$voturi))
+  then
+    voturi=$((${array[${!i}]}))
+    candidat=${!i}
+  fi
+done
+
+echo $candidat $voturi
