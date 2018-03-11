c5179aa31aa8cd3b16d11b2cc322ceb69b9a1cb2
VladuZ <vladuz98@gmail.com>
2018-03-11 22:49:42 +0200

    restructure

--- I.2/SO/lab3.ListParameters.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ((i=1; i<=$#; i++))
-do
-  echo "param_$i = ${!i}"
-done
--- I.2/SO/lab4.11.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-string1=$(cat /etc/passwd | grep :$1: | cut -f 1 -d ':')
-string2=$(id $string1 | cut -f 3 -d ' ' | cut -f 2 -d '=')
-string1=$(echo $string1 | tr -s '.' ' ' | tr -s [:lower:] [:upper:])
-
-echo $string1 $string2
