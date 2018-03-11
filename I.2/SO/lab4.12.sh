1d7a373ed75c45bf500c039e62975269f32da0c8
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-03-11 17:56:22 +0200

    Update lab4.12.sh

--- I.2/SO/lab4.12.sh
+++ I.2/SO/lab4.12.sh
@@ -6,7 +6,7 @@ n=$(echo $var1 | wc -w)
 for ((i=1; i<=$n; i++))
 do
   current=$(echo $var1 | cut -f $i -d ' ')
-  if test -r /fenrir/studs/$current/html/index.html -o -r /fenrir/studs/$current/html/index.html -o -r /fenrir/studs/$current/html/index.php
+  if [ -r /fenrir/studs/$current/html/index.html -o -r /fenrir/studs/$current/html/index.html -o -r /fenrir/studs/$current/html/index.php ]
   then
     echo $current
   fi
