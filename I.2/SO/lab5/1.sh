7e34e1b419c5b588c3c83fd6874b34d9cc59f5b8
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-03-16 20:29:11 +0200

    Create 1.sh

---
+++ I.2/SO/lab5/1.sh
@@ -0,0 +1,32 @@
+directoare=0
+fisiere_ascunse=0
+symlinks=0
+
+if [ $# -eq 0 ]
+then
+  read path
+else
+  path=$1
+fi
+
+parcurge()
+{
+    for f in `ls -A $1`
+    do
+      if [ -d $1/$f ]
+      then
+        let directoare++
+        parcurge $1/$f
+      elif [[ -f $1/$f && $f == .* ]]
+      then
+        let fisiere_ascunse++
+      elif [ -L $1/$f ]
+      then
+        let symlinks++
+      fi
+    done
+}
+
+parcurge $path
+
+echo $directoare $fisiere_ascunse $symlinks
