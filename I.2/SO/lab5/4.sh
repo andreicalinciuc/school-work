0e517335912890b78bfe0696c241cfb5aa90f00d
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-03-16 20:14:43 +0200

    Create 4.sh

---
+++ I.2/SO/lab5/4.sh
@@ -0,0 +1,28 @@
+directoare=0
+fisiere_fifo=0
+
+if [ $# -eq 0 ]
+then
+  read path
+else
+  path=$1
+fi
+
+parcurge()
+{
+    for f in `ls -A $1`
+    do
+      if [ -d $1/$f ]
+      then
+        let directoare++
+        parcurge $1/$f
+      elif [ -p $1/$f ]
+      then
+        let fisiere_fifo++
+      fi
+    done
+}
+
+parcurge $path
+
+echo $directoare $fisiere_fifo
