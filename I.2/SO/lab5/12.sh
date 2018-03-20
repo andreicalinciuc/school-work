55900dd8d45123f057aff9df1c6fafc79cccfe99
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-03-20 20:45:56 +0200

    Update 12.sh

--- I.2/SO/lab5/12.sh
+++ I.2/SO/lab5/12.sh
@@ -7,9 +7,10 @@ fi
 
 echo > ~/html/imagini.html
 
-for f in `find ~/html \( -name "*.jpg" -or -name "*.gif" -or -name "*.png" \) -printf "%p:%t\n" | sort -k 2 -t ':' | cut -f 1 -d ':'`
+for f in `find ~/html \( -name "*.jpg" -or -name "*.gif" -or -name "*.png" \) -printf "%p:%t\n" | sort -k 2 -t ':' | cut -f 1 -d ':' | tr -s ' ' '#'`
 do
-  echo "<a href="${f#/fenrir/studs/vlad.bujoreanu/html/}">$(basename $f)</a><br>" >> ~/html/imagini.html
+  f=$(echo $f | tr -s '#' ' ')
+  echo "<a href=\"${f#/fenrir/studs/vlad.bujoreanu/html/}\">$(basename "$f")</a><br>" >> ~/html/imagini.html
 done
 
 chmod 774 ~/html/imagini.html
