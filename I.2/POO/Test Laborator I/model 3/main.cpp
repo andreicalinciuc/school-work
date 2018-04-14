dc08dcac428043e39feeebdb4f05e8fbc4a46fb3
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-04-14 21:50:40 +0300

    Create main.cpp

---
+++ I.2/POO/Test Laborator I/model 3/main.cpp
@@ -0,0 +1,16 @@
+#include "MyClass.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Lista1[] = {4, 2, 1, 3, 5};
+    Multime m (Lista1, 5);
+    int Lista2[] = {3, 8, 7, 9, 2};
+    Multime n (Lista2, 5);
+    for (int i = 0; i < m.GetSize(); ++i) cout << m[i] << " "; cout << endl;
+    m.Sort (Compare);
+    for (int i = 0; i < m.GetSize(); ++i) cout << m[i] << " "; cout << endl;
+    cout << m.Min() << endl << m.Max() << endl << (double) m << endl << (m & n).GetSize() << endl << (m | n).GetSize();
+}
