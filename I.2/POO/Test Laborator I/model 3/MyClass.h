a34e9b9f68a5745fbe42a74818d57ac0cfea6b6e
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-04-14 21:50:32 +0300

    Create MyClass.h

---
+++ I.2/POO/Test Laborator I/model 3/MyClass.h
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cstring>
+
+int Compare (int, int);
+
+class Multime
+{
+    public:
+    int Numere[100];
+    int Count;
+  public:
+    Multime();
+    Multime (int* Lista, int size);
+    int Max();
+    int Min();
+    void Sort (int (*Compare) (int, int));
+    int GetSize();
+    int& operator [] (int index);
+    operator double ();
+    friend Multime operator & (Multime&, Multime&);
+    friend Multime operator | (Multime&, Multime&);
+};
