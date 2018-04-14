d55d8392e73ac5bad8811c31887a2694b4b9462f
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-04-14 23:45:49 +0300

    Update main.cpp

--- I.2/POO/Test Laborator I/model 3/main.cpp
+++ I.2/POO/Test Laborator I/model 3/main.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+int MyCompareFunction (int a, int b)
+{
+    return a > b;
+}
+
 int main()
 {
     int Lista1[] = {4, 2, 1, 3, 5};
@@ -10,7 +15,7 @@ int main()
     int Lista2[] = {3, 8, 7, 9, 2};
     Multime n (Lista2, 5);
     for (int i = 0; i < m.GetSize(); ++i) cout << m[i] << " "; cout << endl;
-    m.Sort (Compare);
+    m.Sort (MyCompareFunction);
     for (int i = 0; i < m.GetSize(); ++i) cout << m[i] << " "; cout << endl;
     cout << m.Min() << endl << m.Max() << endl << (double) m << endl << (m & n).GetSize() << endl << (m | n).GetSize();
 }
