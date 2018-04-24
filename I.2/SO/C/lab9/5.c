ba3a034090efbe45ed30dc192416864b3ea0200f
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-04-25 01:27:55 +0300

    Update 5.c

--- I.2/SO/C/lab9/5.c
+++ I.2/SO/C/lab9/5.c
@@ -9,21 +9,21 @@ void creeaza_fiu (int i)
     if (i < n)
     {
         pid = fork();
-	    if (pid != 0)
-	    {
-	        wait (&codterm);
-	        printf("Sunt procesul %d, avand PID-ul: %d, parintele are PID-ul: %d, iar fiul creat are PID-ul: %d si s-a terminat cu codul: %d.\n", i, getpid(), getppid(), pid, WEXITSTATUS(codterm));
-	        exit (i);
-  	    }
+        if (pid != 0)
+        {
+            wait (&codterm);
+            printf("Sunt procesul %d, avand PID-ul: %d, parintele are PID-ul: %d, iar fiul creat are PID-ul: %d si s-a terminat cu codul: %d.\n", i, getpid(), getppid(), pid, WEXITSTATUS(codterm));
+            exit (i);
+        }
         else
-	    {
-	        creeaza_fiu (i+1);
-	    }
+        {
+            creeaza_fiu (i+1);
+        }
     }
     else
     {
         printf ("Sunt procesul ultim, %d din lantul de procese, avand PID-ul: %d, iar parintele meu este procesul cu PID-ul: %d.\n", n, getpid(), getppid());
-	    exit (n);
+        exit (n);
     }
 }
 
