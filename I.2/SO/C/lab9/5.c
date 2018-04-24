b82bdf2980a4c838c32e46acc80239c5f77b2afd
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-04-25 01:22:05 +0300

    Create 5.c

---
+++ I.2/SO/C/lab9/5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/wait.h>
+
+int n;
+
+void creeaza_fiu (int i)
+{
+    int pid, codterm;
+    if (i < n)
+    {
+        pid = fork();
+	    if (pid != 0)
+	    {
+	        wait (&codterm);
+	        printf("Sunt procesul %d, avand PID-ul: %d, parintele are PID-ul: %d, iar fiul creat are PID-ul: %d si s-a terminat cu codul: %d.\n", i, getpid(), getppid(), pid, WEXITSTATUS(codterm));
+	        exit (i);
+  	    }
+        else
+	    {
+	        creeaza_fiu (i+1);
+	    }
+    }
+    else
+    {
+        printf ("Sunt procesul ultim, %d din lantul de procese, avand PID-ul: %d, iar parintele meu este procesul cu PID-ul: %d.\n", n, getpid(), getppid());
+	    exit (n);
+    }
+}
+
+int main (int argc, char** argv)
+{
+    printf ("Introduceti numarul de procese: ");
+    scanf ("%d", &n);
+    creeaza_fiu (1);
+}
