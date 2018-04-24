6d86e67ea17e8d0bd35f762dd738ac01887313cc
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-04-24 22:17:11 +0300

    Update 3.c

--- I.2/SO/C/lab8/3.c
+++ I.2/SO/C/lab8/3.c
@@ -2,21 +2,19 @@
 
 int main (int argc, char** argv)
 {
-    int n, i;
-    int pid;
+    int n, i, pid;
     printf("Introduceti numarul de fii: ");
     scanf("%d", &n);
     printf("Sunt procesul initial cu PID-ul: %d, iar parintele meu este procesul cu PID-ul: %d.\n", getpid(), getppid());
     for (i=1; i<=n; i++)
     {
         pid = fork();
-	    if (pid == 0)
-	    {
-	        printf("Sunt procesul fiu %d, avand PID-ul: %d, iar parintele meu este procesul cu PID-ul: %d.\n", i, getpid(), getppid());
-	        return 0;
-  	    }
+        if (pid == 0)
+        {
+            printf("Sunt procesul fiu %d, avand PID-ul: %d, iar parintele meu este procesul cu PID-ul: %d.\n", i, getpid(), getppid());
+            return 0;
+        }
     }
-    for (i=1; i<=n; i++) wait (NULL);
+    for (i=1; i<=n; i++) wait ();
     return 0;
 }
-
