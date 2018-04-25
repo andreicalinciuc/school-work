78d94e544434b4b360b40f28d00e694ca161cb43
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-04-25 18:04:53 +0300

    Update 2.cpp

--- I.2/POO/lab8/2.cpp
+++ I.2/POO/lab8/2.cpp
@@ -2,16 +2,14 @@
 #include <vector>
 #include <functional>
 
-using namespace std;
-
 template <class T>
-void Sort (vector <T> &lista, std::function<int (T &e1, T &e2)> Compare)
+void Sort (std::vector <T> &lista, std::function<int (T &e1, T &e2)> Compare)
 {
     for (unsigned int i = 0; i < lista.size() - 1; i++)
     {
         for (unsigned int j = i + 1; j < lista.size(); j++)
         {
-            if (Compare (lista[i], lista[j])) swap (lista[i], lista[j]);
+            if (Compare (lista[i], lista[j])) std::swap (lista[i], lista[j]);
         }
     }
 }
@@ -19,17 +17,17 @@ void Sort (vector <T> &lista, std::function<int (T &e1, T &e2)> Compare)
 int main()
 {
     int i, n, x;
-    vector <int> v;
-    cin >> n;
+    std::vector <int> v;
+    std::cin >> n;
     for (i = 0; i < n; i++)
     {
-        cin >> x;
+        std::cin >> x;
         v.push_back (x);
     }
     std::function <int (int&, int&)> cmp = [] (int &e1, int &e2) -> int {return e1 > e2;};
     Sort(v, cmp);
     for (i = 0; i < n; i++)
     {
-        cout << v[i] << " ";
+        std::cout << v[i] << " ";
     }
 }
