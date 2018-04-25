634e972c51229f2127c4ccf57276c9154d056dd1
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-04-26 02:14:37 +0300

    Update 1.c

--- I.2/SO/C/lab9/1.c
+++ I.2/SO/C/lab9/1.c
@@ -26,3 +26,63 @@ int main()
     }
     return 0;
 }
+
+//cod de la Alexandru Rusu care inca nu merge (bucla infinita?) dar pare ok
+/*
+#include <stdio.h>
+
+int main()
+{
+	int pid;
+	pid = fork();
+
+/*	if (pid == 0)
+	{
+		sleep(3);
+		printf("Copil %d", getpid());
+		printf("\n");
+	}
+	else
+	{
+		printf("Parinte %d", pid);
+		printf("\n");
+	}
+	*/
+
+	if (pid == 0)
+	{
+		com ("fisier.txt", "fisier2.txt"); //com ("fiu", "parinte");
+		//printf("\n");
+	}
+	else
+	{
+		com ("fisier2.txt", "fisier.txt"); //com ("parinte", "fiu");
+		//printf("\n");
+	}
+}
+
+void com(char *fisier, char *other)
+{
+	FILE *f1=fopen(fisier, "r");
+	char l[250];
+	char flag[20];
+
+	while (!feof(f1))
+	{
+		do 
+		{
+			FILE *f=fopen("fisier3.txt", "r");
+			fgets(flag, 20, f);
+			close(f);
+		}while(strcmp(flag, fisier));
+
+		fgets(l, 250, f1);
+		printf ("%s \n", l);
+		//sleep(1);
+		FILE *f=fopen ("fisier3.txt", "w");
+		fprintf("%s\n", other);
+		fclose(f);
+	}
+
+}
+*/
