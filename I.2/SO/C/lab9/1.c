b595bf96f6fda466d5531a5506aca0bd9b36510d
VladuZ <vladuz98@gmail.com>
2018-04-27 00:41:39 +0300

    new files

--- I.2/SO/C/lab9/1.c
+++ I.2/SO/C/lab9/1.c
@@ -1,75 +1,41 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
 
-void dialog_tata()
+void dialog (char* this, char* other)
 {
-
-}
-
-void dialog_fiu()
-{
-
+    FILE *f1, *f2;
+    char s[256], flag[256];
+    f1 = fopen (this, "r");
+    while (!feof (f1))
+    {
+        do
+        {
+            f2 = fopen ("selector.txt", "r");
+            fgets (flag, 256, f2);
+            fclose (f2);
+        } while (strcmp (flag, this));
+        fgets (s, 256, f1);
+        s[strcspn(s, "\n")] = 0; //smecheria da? da ma da
+        printf ("%s (%s)\n", s, this);
+        f2 = fopen ("selector.txt", "w");
+        fprintf (f2, "%s", other);
+        fclose (f2);
+    }
 }
 
 int main()
 {
+    FILE *f = fopen ("selector.txt", "w");
+    fprintf (f, "fisier1.txt");
+    fclose (f);
     int pid;
     pid = fork();
     if (pid == 0)
     {
-        dialog_fiu();
+        dialog ("fisier1.txt", "fisier2.txt");
     }
     else
     {
-        dialog_tata();
+        dialog ("fisier2.txt", "fisier1.txt");
     }
     return 0;
 }
-
-//cod de la Alexandru Rusu care inca nu merge (bucla infinita?) dar pare ok
-/*
-#include <stdio.h>
-
-int main()
-{
-	int pid;
-	pid = fork();
-
-	if (pid == 0)
-	{
-		com ("fisier.txt", "fisier2.txt"); //com ("fiu", "parinte");
-		//printf("\n");
-	}
-	else
-	{
-		com ("fisier2.txt", "fisier.txt"); //com ("parinte", "fiu");
-		//printf("\n");
-	}
-}
-
-void com(char *fisier, char *other)
-{
-	FILE *f1=fopen(fisier, "r");
-	char l[250];
-	char flag[20];
-
-	while (!feof(f1))
-	{
-		do 
-		{
-			FILE *f=fopen("fisier3.txt", "r");
-			fgets(flag, 20, f);
-			close(f);
-		}while(strcmp(flag, fisier));
-
-		fgets(l, 250, f1);
-		printf ("%s \n", l);
-		//sleep(1);
-		FILE *f=fopen ("fisier3.txt", "w");
-		fprintf("%s\n", other);
-		fclose(f);
-	}
-
-}
-*/
