60e055f2c3c6d1274c473389245bcb5e8b6ecde2
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-03 01:08:33 +0300

    Update 1.R

--- I.2/PS/lab3/1.R
+++ I.2/PS/lab3/1.R
@@ -1,4 +1,4 @@
-M = matrix ( c ( 1,    2,   3,   4,
+m = matrix ( c ( 1,    2,   3,   4,
                0.35, 0.05, 0.15, 0.45),
                nrow = 2, ncol = 4, byrow = TRUE)
 
@@ -15,4 +15,4 @@ Ex1 = function (M)
   return (M[1, i+1]);
 }
 
-#Ex1 (M)
+#Ex1 (m)
