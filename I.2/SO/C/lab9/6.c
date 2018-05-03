7e6722648a457fd377f8cdc5757cfc1d18367b16
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-04 00:05:37 +0300

    Update 6.c

--- I.2/SO/C/lab9/6.c
+++ I.2/SO/C/lab9/6.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
-int n, k;
+int n, k, count = 1;
 
 void creeaza_fii (int i)
 {   
-    int pid, codterm;
+    int pid[101], codterm[101], j;
     if (i < n)
     {
-        printf("Sunt procesul %d, avand PID-ul: %d, parintele are PID-ul: %d, iar fiul creat are PID-ul: %d si s-a terminat cu codul: %d.\n", i, getpid(), getppid(), pid, WEXITSTATUS(codterm));
-        for (int j=1; j<=k; j++)
+        for (j=1; j<=k; j++)
         {
-            pid = fork();
-            if (pid != 0)
+            pid[j] = fork();
+            if (pid[j] == 0)
             {
-                //for (int q=1; q<=k; q++) wait (&codterm);
-            }
-            else
-            {
-                creeaza_fii (i+1);
+                count = j;
                 break;
             }
+            else wait (&codterm[j]);
         }
+        if (j == k+1)
+        {
+            printf ("Sunt procesul %d de pe nivelul %d, PID: %d, PID parinte: %d, PID fii: ", count, i, getpid(), getppid());
+            for (int q=1; q<=k; q++) printf ("%d, ", pid[q]);
+            printf ("si s-au terminat cu codurile: ");
+            for (int q=1; q<=k; q++) printf ("%d, ", WEXITSTATUS (codterm[q]));
+            printf ("\n");
+            exit (count);
+        }
+        else
+        {
+            creeaza_fii (i+1);
+        } 
     }
     else
-    {
-        printf ("Sunt procesul ultim, %d din lantul de procese, avand PID-ul: %d, iar parintele meu este procesul cu PID-ul: %d.\n", n, getpid(), getppid());
-        exit (n);
+    { 
+        printf ("Sunt procesul fara fii %d de pe nivelul %d, PID: %d, PID parinte: %d\n", count, i, getpid(), getppid());
+        exit (count);
     }
 }
 
