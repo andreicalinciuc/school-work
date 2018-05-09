9a1475eaecd0c011957aa701967456f2b9efbcdc
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-10 00:35:52 +0300

    Create 1.R

---
+++ I.2/PS/lab4/1.R
@@ -0,0 +1,28 @@
+sphere_volume = function (N)
+{
+  N_C = 0;
+  for (i in 1:N)
+  {
+    x = runif(1, -1, 1);
+    y = runif(1, -1, 1);
+    z = runif(1, -1, 1);
+    if (x*x + y*y + z*z <= 1)
+      N_C = N_C + 1;
+  }
+  return (8 * N_C / N);
+}
+
+margin_of_error = function (MC_value, actual_value)
+{
+  abs_error = abs (MC_value - actual_value);
+  rel_error = abs (abs_error / actual_value);
+  print (MC_value);
+  print (actual_value);
+  print (abs_error);
+  print (rel_error);
+}
+
+pi = 3.14159265358;
+actual_volume = 4 * pi / 3;
+
+margin_of_error (sphere_volume (50000), actual_volume);
