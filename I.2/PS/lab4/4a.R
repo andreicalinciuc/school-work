195e9e38f05a7c83f539486c44c8f3aa2d8d6979
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-10 01:31:38 +0300

    Create 4a.R

---
+++ I.2/PS/lab4/4a.R
@@ -0,0 +1,27 @@
+MC_integration = function (N)
+{
+  sum = 0;
+  for (i in 1:N)
+  {
+    u = runif (1, 0, pi);
+    sum = sum + cos(u) * cos(u);
+  }
+  return (pi * sum / N);
+}
+
+margin_of_error = function (MC_value, actual_value)
+{
+  abs_error = abs (MC_value - actual_value);
+  rel_error = abs (abs_error / actual_value);
+  print (MC_value);
+  print (actual_value);
+  print (abs_error);
+  print (rel_error);
+}
+
+actual_result = function()
+{
+  return (pi / 2);
+}
+
+margin_of_error (MC_integration (50000), actual_result());
