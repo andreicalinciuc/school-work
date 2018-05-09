2f5a2e53ca25d970f3e509452e9c6ad48428c15c
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-10 01:19:23 +0300

    Create 3.R

---
+++ I.2/PS/lab4/3.R
@@ -0,0 +1,33 @@
+parabola_area = function (N)
+{
+  N_C = 0;
+  for (i in 1:N)
+  {
+    x = runif(1, -1, 3);
+    y = runif(1, 0, 4);
+    if (y <= -(x*x) + 2*x + 3)
+      N_C = N_C + 1;
+  }
+  return (16 * N_C / N);
+}
+
+margin_of_error = function (MC_value, actual_value)
+{
+  abs_error = abs (MC_value - actual_value);
+  rel_error = abs (abs_error / actual_value);
+  print (MC_value);
+  print (actual_value);
+  print (rel_error);
+}
+
+integrate = function (x)
+{
+  return (-(x*x*x)/3 + x*x + 3*x);
+}
+
+actual_area = function()
+{
+  return (integrate (3) - integrate (-1));
+}
+
+margin_of_error (parabola_area (50000), actual_area());
