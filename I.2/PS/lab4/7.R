616dc7d33d1089f743a88bd1c8109a83daeacd47
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-10 02:00:26 +0300

    Create 7.R

---
+++ I.2/PS/lab4/7.R
@@ -0,0 +1,25 @@
+Nr_days = function() {
+  nr_days = 1;
+  last_errors = c (11, 16, 18);
+  nr_errors = 11;
+  while (nr_errors > 0)
+  {
+    lambda = min (last_errors);
+    nr_errors = rpois (1, lambda);
+    last_errors = c (nr_errors, last_errors[1], last_errors[2]) ;
+    nr_days = nr_days + 1;
+  }
+  return (nr_days);
+}
+
+MC_Nr_days = function (N)
+{
+  s = 0;
+  for (i in 1:N)
+  {
+    s = s + Nr_days();
+  }
+  return (s / N);
+}
+
+print (MC_Nr_days (10000));
