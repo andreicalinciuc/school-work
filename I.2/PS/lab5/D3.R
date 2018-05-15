2e7463b7809510e39fdb47749462e3cc08d07e2c
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-15 13:39:22 +0300

    Create D3.R

---
+++ I.2/PS/lab5/D3.R
@@ -0,0 +1,35 @@
+test_proportion = function (alfa, n, succese, p0)
+{
+  p_prim = succese / n;
+  z = (p_prim - p0) / sqrt (p0 * (1 - p0) / n);
+  critical_z = qnorm (1 - alfa, 0, 1);
+  print (z);
+  print (critical_z);
+  if (z > 0)
+  {
+    print ("asimetrica la dreapta");
+    if (z > critical_z)
+    {
+      print ("ipoteza nula respinsa");
+    }
+    else
+    {
+      print ("nu exista suficiente dovezi");
+    }
+  }
+  if (z < 0)
+  {
+    print ("asimetrica la stanga");
+    if (z < critical_z)
+    {
+      print ("ipoteza nula respinsa");
+    }
+    else
+    {
+      print ("nu exista suficiente dovezi");
+    }
+  }
+}
+
+test_proportion (0.1, 112, 14, 0.1)
+test_proportion (0.5, 112, 14, 0.1)
