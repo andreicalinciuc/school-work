ba6a106d48d43c8fa17481ee56d4a8b60a62bc36
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-15 12:40:33 +0300

    Create IV5.R

---
+++ I.2/PS/lab5/IV5.R
@@ -0,0 +1,4 @@
+date_esantion = c (12, 11, 12, 10, 11, 12, 13, 12, 11, 11, 13, 14, 10);
+t_conf_interval (length (date_esantion), mean (date_esantion), 0.1, sd (date_esantion))
+t_conf_interval (length (date_esantion), mean (date_esantion), 0.05, sd (date_esantion))
+t_conf_interval (length (date_esantion), mean (date_esantion), 0.01, sd (date_esantion))
