0b18fd1583deb65cd54eca9e7e485312e1344fc1
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-20 18:52:52 +0300

    Update 4.c

--- I.2/SO/C/model site/4.c
+++ I.2/SO/C/model site/4.c
@@ -1,30 +1,60 @@
 #include <stdio.h>
 
+int p1[2], p2[2], pf[2];
+
+void master_work()
+{
+    int suma_partiala1 = 0, suma_partiala2 = 0, nr = 0;
+    while (scanf ("%d", &nr) != EOF)
+    {
+        if (nr < 0) write (p1[1], &nr, sizeof (int));
+        if (nr > 0) write (p2[1], &nr, sizeof (int));
+    }
+    close (p1[1]); close (p2[1]);
+    read (pf[0], &suma_partiala1, sizeof (int));
+    read (pf[0], &suma_partiala2, sizeof (int));
+    close (pf[0]);
+    printf ("Sunt procesul tata cu PID %d si am primit sumele partiale %d si %d, iar suma lor este %d\n", getpid(), suma_partiala1, suma_partiala2, suma_partiala1 + suma_partiala2);
+}
+
+void slave_work (int fdi, int fdo)
+{
+    int suma_partiala = 0, numar[100], k = 0, nr = 0, i;
+    while (read (fdi, &nr, sizeof (int)))
+    {
+        numar[++k] = nr;
+        suma_partiala += nr;
+    }
+    close (fdi);
+    printf ("Sunt procesul fiu cu PID %d si am secventa ", getpid());
+    for (i = 1; i <= k; ++i) printf ("%d ", numar[i]);
+    printf ("si suma partiala %d\n", suma_partiala);
+    write (fdo, &suma_partiala, sizeof (int));
+    close (fdo);
+}
+
 int main()
 {
-    int p[2];
-    pipe (p);
+    pipe (p1);
+    pipe (p2);
+    pipe (pf);
     if (fork() == 0)
     {
-        FILE *fin = fdopen (p[0], "r");
-        close (p[1]);
-        //while citire != eof
-        //calculare suma 1
+        close (p1[1]);
+        close (p2[0]); close (p2[1]);
+        close (pf[0]);
+        slave_work (p1[0], pf[1]);
+        return 0;
     }
-    else if (fork() == 0)
+    if (fork() == 0)
     {
-        FILE *fout = fdopen (p[1], "w");
-        close (p[0]);
-        //while citire != eof
-        //calculare suma 2
+        close (p1[0]); close (p1[1]);
+        close (p2[1]);
+        close (pf[0]);
+        slave_work (p2[0], pf[1]);
+        return 0;
     }
-    else
-    {
-        //citirea
-        wait(NULL);
-        wait(NULL);
-        //suma 1
-        //suma 2
-        //suma sumelor
-    }
+    close (p1[0]); close (p2[0]); close (pf[1]);
+    master_work();
+    return 0;
 }
