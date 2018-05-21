f28c8374a84814be1f11fadc169a7f31b3cc4154
Vlad Bujoreanu <32713714+VladuZ@users.noreply.github.com>
2018-05-21 20:43:17 +0300

    Update 3.c

--- I.2/SO/C/model site/3.c
+++ I.2/SO/C/model site/3.c
@@ -1,3 +1,5 @@
+//Fie programul C de mai jos. El contine (macar) 3 erori sintactice si o greseala logica. Corectati-le, astfel incat programul sa poata fi executat si sa produca la executie afisarea pe ecran de catre procesul fiu a textului primit de la parinte.
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
